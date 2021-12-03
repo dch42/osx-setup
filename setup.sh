63c881cd325cfcba5235c55f52c4a8dabd53f7f1
ddch42 <85430824+dch42@users.noreply.github.com>
2021-12-02 20:54:57 -0600

    formulae

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 clear
+cat fig.txt
 printf "OSX Setup Script\n\n"
+printf "This script will install various packages on your system.\n\n"
+read -p "Press 'CTRL+C' to quit, or any key to continue..."
+
 printf "Checking for Homebrew 🍺 installation...\n"
 
 while ! command -v brew &> /dev/null
@@ -24,28 +28,59 @@ brew update
 
 homebrew_formulae=(
     "git"
+    "wget"
     "python"
+    "pylint"
     "figlet"
     "magic-wormhole"
     "ffmpeg"
     "imagemagick"
     "cowsay"
+    "irssi"
     "sqlcipher"
     "sqlite"
     "sl"
 )
 
 printf "\nInstalling packages...\n"
-for formulae in "${homebrew_formulae[@]}"
+for formula in "${homebrew_formulae[@]}"
 do
-    printf "==> $formulae\n"
+    printf "==> $formula\n"
 done
 
-for formulae in "${homebrew_formulae[@]}"
+for formula in "${homebrew_formulae[@]}"
 do
-    printf "\nInstalling $formulae...\n"
-    brew install "$formulae"
+    printf "\nInstalling $formula...\n"
+    brew install "$formula"
+    printf "\e[5m\e[36m==>\e[0m $formula installed. ✅ \n\n"
 done
 
 figlet ~success~
 
+homebrew_casks=(
+    "little-snitch"
+    "firefox"
+    "gimp"
+    "libreoffice"
+    "adium"
+    "macfuse"
+    "veracrypt"
+    "signal"
+    "visual-studio-code"
+)
+
+printf "\nInstalling casks...\n"
+for cask in "${homebrew_casks[@]}"
+do
+    printf "==> $cask\n"
+done
+
+for cask in "${homebrew_casks[@]}"
+do
+    printf "\nInstalling $cask...\n"
+    brew install --cask "$cask"
+    printf "\e[5m\e[36m==>\e[0m $cask installed. ✅ \n\n"
+
+done
+
+brew cleanup
