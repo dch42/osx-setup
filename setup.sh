b3e87dc98281a52da76b754cf9b5b6282a2c9aae
ddch42 <85430824+dch42@users.noreply.github.com>
2021-12-04 15:28:46 -0600

    switch from grep -c to grep -q

--- setup.sh
+++ setup.sh
@@ -70,62 +70,62 @@ read -p "Press 'CTRL+C' to quit, or any key to continue..."
 # |_.__/|_|  \___| \_/\_/ 
 ###############################################################################
 
-# printf "Checking for Homebrew 🍺 installation...\n"
+printf "Checking for Homebrew 🍺 installation...\n"
 
-# while ! command -v brew &> /dev/null
-# do
-#     printf "==> Homebrew not installed. ❌\n\n"
-#     printf "Installing Homebrew 🍺...\n"
-#     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-# done
+while ! command -v brew &> /dev/null
+do
+    printf "==> Homebrew not installed. ❌\n\n"
+    printf "Installing Homebrew 🍺...\n"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+done
 
-# if command -v brew &> /dev/null
-# then
-#     printf "\e[5m\e[36m==>\e[0m Homebrew installed. ✅ \n\n"
-# fi
+if command -v brew &> /dev/null
+then
+    printf "\e[5m\e[36m==>\e[0m Homebrew installed. ✅ \n\n"
+fi
 
-# printf "Disabling Homebrew analytics...\n"
-# brew analytics off
-# brew analytics state
+printf "Disabling Homebrew analytics...\n"
+brew analytics off
+brew analytics state
 
-# brew update
+brew update
 
-# homebrew_formulae=(
-#     "git"
-#     "wget"
-#     "python"
-#     "pylint"
-#     "figlet"
-#     "magic-wormhole"
-#     "ffmpeg"
-#     "imagemagick"
-#     "cowsay"
-#     "irssi"
-#     "sqlcipher"
-#     "sqlite"
-#     "sl"
-# )
+homebrew_formulae=(
+    "git"
+    "wget"
+    "python"
+    "pylint"
+    "figlet"
+    "magic-wormhole"
+    "ffmpeg"
+    "imagemagick"
+    "cowsay"
+    "irssi"
+    "sqlcipher"
+    "sqlite"
+    "sl"
+)
 
-# homebrew_casks=(
-#     "little-snitch"
-#     "firefox"
-#     "gimp"
-#     "libreoffice"
-#     "adium"
-#     "macfuse"
-#     "veracrypt"
-#     "signal"
-#     "visual-studio-code"
-# )
+homebrew_casks=(
+    "little-snitch"
+    "firefox"
+    "gimp"
+    "libreoffice"
+    "adium"
+    "macfuse"
+    "veracrypt"
+    "signal"
+    "visual-studio-code"
+)
 
-# printf "\nInstalling packages...\n"
-# install_all "${homebrew_formulae[@]}" "$(brew install)"
+printf "\nInstalling packages...\n"
+install_all "${homebrew_formulae[@]}" "$(brew install)"
 
-# printf "\nInstalling casks...\n"
-# install_all "${homebrew_casks[@]}" "$(brew install --cask)"
+printf "\nInstalling casks...\n"
+install_all "${homebrew_casks[@]}" "$(brew install --cask)"
 
 
-# brew cleanup
+brew cleanup
 
 ###############################################################################
 #                               __ _       
